ec964fe8d27dce90abc74dd419f997c8af4239a2
Manjunath Badiger <m1064478@mindtree.com>
2021-06-10 18:01:07 +0530

    kellogs commited

---
+++ core/src/main/java/com/mindtree/test/core/models/BannerCard.java
@@ -0,0 +1,17 @@
+package com.mindtree.test.core.models;
+
+public interface BannerCard {
+	
+	public String getTitle();
+	
+	public String getDescription();
+	
+	public String getFilePath();
+	
+	public String getPrepTime();
+	
+	public String getTotalTime();
+	
+	public String getServings();
+
+}
